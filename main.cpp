10c1f7f9093813038901fd66dcd77873dc64241d
ntsh-oni <teamnutshell.dev@gmail.com>
2023-01-28 17:54:03 +0100

    initial application

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "external/Core/src/ntshengn_core.h"
+
+int main() {
+	NtshEngn::Core core;
+
+	// Initialize
+	core.init();
+
+	// Loop
+	core.update();
+
+	// Destroy
+	core.destroy();
+
+	return 0;
+}
