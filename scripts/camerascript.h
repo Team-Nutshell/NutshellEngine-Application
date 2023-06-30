dcd71bf49fa13e40e842e111b79fac709760f558
ntsh-oni <teamnutshell.dev@gmail.com>
2023-06-30 17:33:00 +0200

    update according to window module interface

--- scripts/camerascript.h
+++ scripts/camerascript.h
@@ -6,12 +6,12 @@ struct CameraScript : NtshEngn::Script {
 	NTSHENGN_SCRIPT(CameraScript);
 
 	void init() {
-		if (windowModule && windowModule->isOpen(NTSHENGN_MAIN_WINDOW)) {
-			windowModule->setCursorVisibility(NTSHENGN_MAIN_WINDOW, !m_mouseMiddleMode);
+		if (windowModule && windowModule->isOpen(windowModule->getMainWindowID())) {
+			windowModule->setCursorVisibility(windowModule->getMainWindowID(), !m_mouseMiddleMode);
 
-			m_prevMouseX = windowModule->getWidth(NTSHENGN_MAIN_WINDOW) / 2;
-			m_prevMouseY = windowModule->getHeight(NTSHENGN_MAIN_WINDOW) / 2;
-			windowModule->setCursorPosition(NTSHENGN_MAIN_WINDOW, m_prevMouseX, m_prevMouseY);
+			m_prevMouseX = windowModule->getWidth(windowModule->getMainWindowID()) / 2;
+			m_prevMouseY = windowModule->getHeight(windowModule->getMainWindowID()) / 2;
+			windowModule->setCursorPosition(windowModule->getMainWindowID(), m_prevMouseX, m_prevMouseY);
 
 			const NtshEngn::Transform transform = ecs->getComponent<NtshEngn::Transform>(entityID);
 			nml::vec3 cameraRotation = nml::vec3(transform.rotation[0], transform.rotation[1], transform.rotation[2]);
@@ -22,14 +22,14 @@ struct CameraScript : NtshEngn::Script {
 	}
 
 	void update(double dt) {
-		if (windowModule && windowModule->isOpen(NTSHENGN_MAIN_WINDOW)) {
-			if (windowModule->getKeyState(NTSHENGN_MAIN_WINDOW, NtshEngn::InputKeyboardKey::R) == NtshEngn::InputState::Pressed) {
+		if (windowModule && windowModule->isOpen(windowModule->getMainWindowID())) {
+			if (windowModule->getKeyState(windowModule->getMainWindowID(), NtshEngn::InputKeyboardKey::R) == NtshEngn::InputState::Pressed) {
 				m_mouseMiddleMode = !m_mouseMiddleMode;
-				windowModule->setCursorVisibility(NTSHENGN_MAIN_WINDOW, !m_mouseMiddleMode);
+				windowModule->setCursorVisibility(windowModule->getMainWindowID(), !m_mouseMiddleMode);
 				if (m_mouseMiddleMode) {
-					m_prevMouseX = windowModule->getWidth(NTSHENGN_MAIN_WINDOW) / 2;
-					m_prevMouseY = windowModule->getHeight(NTSHENGN_MAIN_WINDOW) / 2;
-					windowModule->setCursorPosition(NTSHENGN_MAIN_WINDOW, m_prevMouseX, m_prevMouseY);
+					m_prevMouseX = windowModule->getWidth(windowModule->getMainWindowID()) / 2;
+					m_prevMouseY = windowModule->getHeight(windowModule->getMainWindowID()) / 2;
+					windowModule->setCursorPosition(windowModule->getMainWindowID(), m_prevMouseX, m_prevMouseY);
 				}
 			}
 
@@ -37,12 +37,12 @@ struct CameraScript : NtshEngn::Script {
 			nml::vec3 cameraRotation = nml::vec3(transform.rotation[0], transform.rotation[1], transform.rotation[2]);
 
 			if (m_mouseMiddleMode) {
-				const int mouseX = windowModule->getCursorPositionX(NTSHENGN_MAIN_WINDOW);
-				const int mouseY = windowModule->getCursorPositionY(NTSHENGN_MAIN_WINDOW);
+				const int mouseX = windowModule->getCursorPositionX(windowModule->getMainWindowID());
+				const int mouseY = windowModule->getCursorPositionY(windowModule->getMainWindowID());
 
-				m_prevMouseX = windowModule->getWidth(NTSHENGN_MAIN_WINDOW) / 2;
-				m_prevMouseY = windowModule->getHeight(NTSHENGN_MAIN_WINDOW) / 2;
-				windowModule->setCursorPosition(NTSHENGN_MAIN_WINDOW, m_prevMouseX, m_prevMouseY);
+				m_prevMouseX = windowModule->getWidth(windowModule->getMainWindowID()) / 2;
+				m_prevMouseY = windowModule->getHeight(windowModule->getMainWindowID()) / 2;
+				windowModule->setCursorPosition(windowModule->getMainWindowID(), m_prevMouseX, m_prevMouseY);
 
 				const float xOffset = (mouseX - m_prevMouseX) * m_mouseSensitivity;
 				const float yOffset = (mouseY - m_prevMouseY) * m_mouseSensitivity;
@@ -65,26 +65,26 @@ struct CameraScript : NtshEngn::Script {
 			const float cameraSpeed = m_cameraSpeed * static_cast<float>(dt);
 
 			nml::vec3 cameraPosition = nml::vec3(transform.position[0], transform.position[1], transform.position[2]);
-			if (windowModule->getKeyState(NTSHENGN_MAIN_WINDOW, NtshEngn::InputKeyboardKey::W) == NtshEngn::InputState::Held) {
+			if (windowModule->getKeyState(windowModule->getMainWindowID(), NtshEngn::InputKeyboardKey::W) == NtshEngn::InputState::Held) {
 				cameraPosition += (cameraRotation * cameraSpeed);
 			}
-			if (windowModule->getKeyState(NTSHENGN_MAIN_WINDOW, NtshEngn::InputKeyboardKey::S) == NtshEngn::InputState::Held) {
+			if (windowModule->getKeyState(windowModule->getMainWindowID(), NtshEngn::InputKeyboardKey::S) == NtshEngn::InputState::Held) {
 				cameraPosition -= (cameraRotation * cameraSpeed);
 			}
-			if (windowModule->getKeyState(NTSHENGN_MAIN_WINDOW, NtshEngn::InputKeyboardKey::A) == NtshEngn::InputState::Held) {
+			if (windowModule->getKeyState(windowModule->getMainWindowID(), NtshEngn::InputKeyboardKey::A) == NtshEngn::InputState::Held) {
 				nml::vec3 t = nml::normalize(nml::vec3(-cameraRotation.z, 0.0, cameraRotation.x));
 				cameraPosition.x -= (t.x * cameraSpeed);
 				cameraPosition.z -= (t.z * cameraSpeed);
 			}
-			if (windowModule->getKeyState(NTSHENGN_MAIN_WINDOW, NtshEngn::InputKeyboardKey::D) == NtshEngn::InputState::Held) {
+			if (windowModule->getKeyState(windowModule->getMainWindowID(), NtshEngn::InputKeyboardKey::D) == NtshEngn::InputState::Held) {
 				nml::vec3 t = nml::normalize(nml::vec3(-cameraRotation.z, 0.0, cameraRotation.x));
 				cameraPosition.x += (t.x * cameraSpeed);
 				cameraPosition.z += (t.z * cameraSpeed);
 			}
-			if (windowModule->getKeyState(NTSHENGN_MAIN_WINDOW, NtshEngn::InputKeyboardKey::Space) == NtshEngn::InputState::Held) {
+			if (windowModule->getKeyState(windowModule->getMainWindowID(), NtshEngn::InputKeyboardKey::Space) == NtshEngn::InputState::Held) {
 				cameraPosition.y += cameraSpeed;
 			}
-			if (windowModule->getKeyState(NTSHENGN_MAIN_WINDOW, NtshEngn::InputKeyboardKey::Shift) == NtshEngn::InputState::Held) {
+			if (windowModule->getKeyState(windowModule->getMainWindowID(), NtshEngn::InputKeyboardKey::Shift) == NtshEngn::InputState::Held) {
 				cameraPosition.y -= cameraSpeed;
 			}
 
