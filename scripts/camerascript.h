b2a8dee941e3452b55657fa5dc293854a26aef53
ntsh-oni <teamnutshell.dev@gmail.com>
2023-09-01 15:17:51 +0200

    use new scripting API

--- scripts/camerascript.h
+++ scripts/camerascript.h
@@ -1,78 +1,70 @@
 #pragma once
-#include "../Core/Common/resources/ntshengn_resources_scripting.h"
-#include "../Core/Common/module_interfaces/ntshengn_window_module_interface.h"
-#include "../Core/Common/utils/ntshengn_utils_math.h"
-#include <cmath>
+#include "../Core/scripting/ntshengn_scripting_api.h"
 
-struct CameraScript : public NtshEngn::Script {
+using namespace NtshEngn;
+struct CameraScript : public ScriptingAPI {
 	NTSHENGN_SCRIPT(CameraScript);
 
 	void init() {
-		if (windowModule && windowModule->isWindowOpen(windowModule->getMainWindowID())) {
-			windowModule->setCursorVisibility(windowModule->getMainWindowID(), !m_mouseMiddleMode);
+		setCursorVisibility(!m_mouseMiddleMode);
 
-			m_prevMouseX = windowModule->getWindowWidth(windowModule->getMainWindowID()) / 2;
-			m_prevMouseY = windowModule->getWindowHeight(windowModule->getMainWindowID()) / 2;
-			windowModule->setCursorPosition(windowModule->getMainWindowID(), m_prevMouseX, m_prevMouseY);
-		}
+		m_prevMouseX = getWindowWidth() / 2;
+		m_prevMouseY = getWindowHeight() / 2;
+		setCursorPosition(m_prevMouseX, m_prevMouseY);
 	}
 
 	void update(double dt) {
 		NTSHENGN_UNUSED(dt);
 
-		if (windowModule && windowModule->isWindowOpen(windowModule->getMainWindowID())) {
-			if (windowModule->getKeyState(windowModule->getMainWindowID(), NtshEngn::InputKeyboardKey::R) == NtshEngn::InputState::Pressed) {
-				m_mouseMiddleMode = !m_mouseMiddleMode;
-				windowModule->setCursorVisibility(windowModule->getMainWindowID(), !m_mouseMiddleMode);
-				if (m_mouseMiddleMode) {
-					m_prevMouseX = windowModule->getWindowWidth(windowModule->getMainWindowID()) / 2;
-					m_prevMouseY = windowModule->getWindowHeight(windowModule->getMainWindowID()) / 2;
-					windowModule->setCursorPosition(windowModule->getMainWindowID(), m_prevMouseX, m_prevMouseY);
-				}
-			}
-
-			NtshEngn::Transform& transform = ecs->getComponent<NtshEngn::Transform>(entityID);
-
-			const NtshEngn::Transform& otherTransform = ecs->getComponent<NtshEngn::Transform>(m_other);
-
+		if (getKeyState(InputKeyboardKey::R) == InputState::Pressed) {
+			m_mouseMiddleMode = !m_mouseMiddleMode;
+			setCursorVisibility(!m_mouseMiddleMode);
 			if (m_mouseMiddleMode) {
-				const int mouseX = windowModule->getCursorPositionX(windowModule->getMainWindowID());
-				const int mouseY = windowModule->getCursorPositionY(windowModule->getMainWindowID());
-
-				m_prevMouseX = windowModule->getWindowWidth(windowModule->getMainWindowID()) / 2;
-				m_prevMouseY = windowModule->getWindowHeight(windowModule->getMainWindowID()) / 2;
-				windowModule->setCursorPosition(windowModule->getMainWindowID(), m_prevMouseX, m_prevMouseY);
-
-				const float xOffset = (mouseX - m_prevMouseX) * m_mouseSensitivity;
-				const float yOffset = (mouseY - m_prevMouseY) * m_mouseSensitivity;
-
-				m_prevMouseX = mouseX;
-				m_prevMouseY = mouseY;
-
-				m_yaw = std::fmod(m_yaw + xOffset, 360.0f);
-				m_pitch = std::max(-89.0f, std::min(89.0f, m_pitch - yOffset));
+				m_prevMouseX = getWindowWidth() / 2;
+				m_prevMouseY = getWindowHeight() / 2;
+				setCursorPosition(m_prevMouseX, m_prevMouseY);
 			}
-
-			m_distance = std::max(0.0f, std::min(m_distance - windowModule->getMouseScrollOffsetY(windowModule->getMainWindowID()), 100.0f));
-
-			float yawRad = m_yaw * toRad;
-			float pitchRad = m_pitch * toRad;
-
-			transform.position.x = std::cos(pitchRad) * std::cos(yawRad);
-			transform.position.y = -std::sin(pitchRad);
-			transform.position.z = std::cos(pitchRad) * std::sin(yawRad);
-			transform.position *= m_distance;
-			transform.position += otherTransform.position;
-			transform.rotation = normalize(otherTransform.position - transform.position);
 		}
+
+		Transform& transform = getEntityComponent<Transform>(entityID);
+
+		const Transform& otherTransform = getEntityComponent<Transform>(m_other);
+
+		if (m_mouseMiddleMode) {
+			const int mouseX = getCursorPositionX();
+			const int mouseY = getCursorPositionY();
+
+			m_prevMouseX = getWindowWidth() / 2;
+			m_prevMouseY = getWindowHeight() / 2;
+			setCursorPosition(m_prevMouseX, m_prevMouseY);
+
+			const float xOffset = (mouseX - m_prevMouseX) * m_mouseSensitivity;
+			const float yOffset = (mouseY - m_prevMouseY) * m_mouseSensitivity;
+
+			m_prevMouseX = mouseX;
+			m_prevMouseY = mouseY;
+
+			m_yaw = std::fmod(m_yaw + xOffset, 360.0f);
+			m_pitch = std::max(-89.0f, std::min(89.0f, m_pitch - yOffset));
+		}
+
+		m_distance = std::max(0.0f, std::min(m_distance - getMouseScrollOffsetY(), 100.0f));
+
+		float yawRad = Math::toRad(m_yaw);
+		float pitchRad = Math::toRad(m_pitch);
+
+		transform.position.x = std::cos(pitchRad) * std::cos(yawRad);
+		transform.position.y = -std::sin(pitchRad);
+		transform.position.z = std::cos(pitchRad) * std::sin(yawRad);
+		transform.position *= m_distance;
+		transform.position += otherTransform.position;
+		transform.rotation = normalize(otherTransform.position - transform.position);
 	}
 
 	void destroy() {
 	}
 
 private:
-	const float toRad = 3.1415926535897932384626433832795f / 180.0f;
-	const float toDeg = 180.0f / 3.1415926535897932384626433832795f;
 
 	bool m_mouseMiddleMode = false;
 
@@ -87,5 +79,5 @@ private:
 
 	float m_distance = 4.0f;
 
-	NtshEngn::Entity m_other = 1;
+	Entity m_other = 1;
 };
--- scripts/cubescript.h
+++ scripts/cubescript.h
@@ -1,39 +1,35 @@
 #pragma once
-#include "../Core/Common/resources/ntshengn_resources_scripting.h"
-#include "../Core/Common/module_interfaces/ntshengn_window_module_interface.h"
-#include "../Core/Common/utils/ntshengn_utils_math.h"
-#include <cmath>
+#include "../Core/scripting/ntshengn_scripting_api.h"
 
-struct CubeScript : public NtshEngn::Script {
+using namespace NtshEngn;
+struct CubeScript : public ScriptingAPI {
 	NTSHENGN_SCRIPT(CubeScript);
 
 	void init() {
 	}
 
 	void update(double dt) {
-		if (windowModule && windowModule->isWindowOpen(windowModule->getMainWindowID())) {
-			NtshEngn::Transform& transform = ecs->getComponent<NtshEngn::Transform>(entityID);
-			NtshEngn::Transform& cameraTransform = ecs->getComponent<NtshEngn::Transform>(m_camera);
-			const float cubeSpeed = m_cubeSpeed * static_cast<float>(dt);
+		Transform& transform = getEntityComponent<Transform>(entityID);
+		Transform& cameraTransform = getEntityComponent<Transform>(m_camera);
+		const float cubeSpeed = m_cubeSpeed * static_cast<float>(dt);
 
-			if (windowModule->getKeyState(windowModule->getMainWindowID(), NtshEngn::InputKeyboardKey::W) == NtshEngn::InputState::Held) {
-				transform.position.x += (cameraTransform.rotation.x * cubeSpeed);
-				transform.position.z += (cameraTransform.rotation.z * cubeSpeed);
-			}
-			if (windowModule->getKeyState(windowModule->getMainWindowID(), NtshEngn::InputKeyboardKey::S) == NtshEngn::InputState::Held) {
-				transform.position.x -= (cameraTransform.rotation.x * cubeSpeed);
-				transform.position.z -= (cameraTransform.rotation.z * cubeSpeed);
-			}
-			if (windowModule->getKeyState(windowModule->getMainWindowID(), NtshEngn::InputKeyboardKey::A) == NtshEngn::InputState::Held) {
-				NtshEngn::Math::vec3 t = NtshEngn::Math::normalize(NtshEngn::Math::vec3(-cameraTransform.rotation.z, 0.0, cameraTransform.rotation.x));
-				transform.position.x -= (t.x * cubeSpeed);
-				transform.position.z -= (t.z * cubeSpeed);
-			}
-			if (windowModule->getKeyState(windowModule->getMainWindowID(), NtshEngn::InputKeyboardKey::D) == NtshEngn::InputState::Held) {
-				NtshEngn::Math::vec3 t = NtshEngn::Math::normalize(NtshEngn::Math::vec3(-cameraTransform.rotation.z, 0.0, cameraTransform.rotation.x));
-				transform.position.x += (t.x * cubeSpeed);
-				transform.position.z += (t.z * cubeSpeed);
-			}
+		if (getKeyState(InputKeyboardKey::W) == InputState::Held) {
+			transform.position.x += (cameraTransform.rotation.x * cubeSpeed);
+			transform.position.z += (cameraTransform.rotation.z * cubeSpeed);
+		}
+		if (getKeyState(InputKeyboardKey::S) == InputState::Held) {
+			transform.position.x -= (cameraTransform.rotation.x * cubeSpeed);
+			transform.position.z -= (cameraTransform.rotation.z * cubeSpeed);
+		}
+		if (getKeyState(InputKeyboardKey::A) == InputState::Held) {
+			Math::vec3 t = Math::normalize(Math::vec3(-cameraTransform.rotation.z, 0.0, cameraTransform.rotation.x));
+			transform.position.x -= (t.x * cubeSpeed);
+			transform.position.z -= (t.z * cubeSpeed);
+		}
+		if (getKeyState(InputKeyboardKey::D) == InputState::Held) {
+			Math::vec3 t = Math::normalize(Math::vec3(-cameraTransform.rotation.z, 0.0, cameraTransform.rotation.x));
+			transform.position.x += (t.x * cubeSpeed);
+			transform.position.z += (t.z * cubeSpeed);
 		}
 	}
 
@@ -43,5 +39,5 @@ struct CubeScript : public NtshEngn::Script {
 private:
 	const float m_cubeSpeed = 0.015f;
 
-	NtshEngn::Entity m_camera = 0;
+	Entity m_camera = 0;
 };
